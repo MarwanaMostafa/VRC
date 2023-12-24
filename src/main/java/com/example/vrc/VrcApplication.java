a79287e14c9290998a3d9ff4fbdc6634735a7161
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-12-24 03:40:31 +0200

    "Initial Commit"

---
+++ src/main/java/com/example/vrc/VrcApplication.java
@@ -0,0 +1,13 @@
+package com.example.vrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vrc/VrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
