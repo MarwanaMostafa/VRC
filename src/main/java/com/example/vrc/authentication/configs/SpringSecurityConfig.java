d31922319d0033be459d6ed783bd7d8591e6effd
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-12-31 11:56:30 +0200

    "chore: separate the endpoints of rooms creation and update & restrict the rooms update according to the owner"

--- src/main/java/com/example/vrc/authentication/configs/SpringSecurityConfig.java
+++ src/main/java/com/example/vrc/authentication/configs/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,11 +21,12 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return
             http
-                    .csrf().disable()
-                    .authorizeHttpRequests().requestMatchers("/sign-up","/login", "/error","/forgot-password","/set-password").permitAll()
-                    .anyRequest().authenticated().and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                    .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-                    .build();
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/h2-console/**", "/api/sign-up", "/api/login", "/error", "/forgot-password","/set-password").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
