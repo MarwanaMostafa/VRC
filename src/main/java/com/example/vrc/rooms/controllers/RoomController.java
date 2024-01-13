c5c064c7776334d26a1d1f497eb0cdd1c4778986
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2024-01-13 08:57:33 +0200

    Revert "Revert "Add Cross""
    This reverts commit bdfef98a6594c6bc68ac9dce340edcf605261b87.

--- src/main/java/com/example/vrc/rooms/controllers/RoomController.java
+++ src/main/java/com/example/vrc/rooms/controllers/RoomController.java
@@ -34,6 +34,8 @@ import com.example.vrc.shared.utilities.UserInputsValidator;
 @RestController
 @RequestMapping("/api/rooms")
 @SecurityRequirement(name = "AddToken")
+@CrossOrigin(origins = "*", allowCredentials = "true")
+
 public class RoomController {
     @Autowired
     private RoomService roomService;
